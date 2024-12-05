f9d0a465bbacf289c977e24a88d9f350a18e2498
artemka <rambler601@yandex.ru>
2024-12-05 18:26:45 +0300

    фиксирую состояние

--- src/counter.js
+++ src/counter.js
@@ -1,7 +1,7 @@
-import { flatten } from "lodash-es";
+
 
 export function setupCounter(element) {
-  console.log(flatten([[[1,2,3,4,5]]]))
+  console.log(([[[1,2,3,4,5]]]))
   let counter = 0
   const setCounter = (count) => {
     counter = count
