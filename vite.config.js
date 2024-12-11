997e4c1e349dc2df6a639514b2b5d3190bd19859
artemka <rambler601@yandex.ru>
2024-12-11 21:29:16 +0300

    sprint_1 last correction

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import {defineConfig} from 'vite';
 import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
-    plugins: [handlebars()]
+    plugins: [handlebars()],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
 })
