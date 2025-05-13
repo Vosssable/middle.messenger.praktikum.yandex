a2e0df8e540591cc4b13416d991c7b0ebb5888e6
artemka <rambler601@yandex.ru>
2025-05-13 21:04:55 +0300

    [pre-commit] - changed

--- src/framework/Block.test.ts
+++ src/framework/Block.test.ts
@@ -1,49 +1,72 @@
 import Block from "./Block"
+import { Indexed } from "../utils/interfaces/frameworkInterfaces"
 
-describe("Тестирование блока", () => {
-  describe("Тест блока", () => {
-    let blockClass: typeof Block
+describe("Тестирование блока компонента", () => {
+  let blockClass: typeof Block
+  const textData = "Inner test text"
 
-    beforeAll(() => {
-      class Button extends Block {
-        constructor(props: any) {
-          super({
-            ...props
-          })
-        }
-
-        render() {
-          return `<div id="div">{{text}}</div>`
-        }
+  beforeAll(() => {
+    class Button extends Block {
+      constructor(props: Indexed) {
+        super({
+          ...props
+        })
       }
 
-      blockClass = Button
-    })
+      render() {
+        return `<div id="div">{{text}}</div>`
+      }
+    }
 
+    blockClass = Button
+  })
+
+  describe("Тест блока", () => {
     it("Рендер пропсов", () => {
-      const textData = "Inner test text"
-      const buttonComponent = new blockClass({ text: textData })
-      const res = (buttonComponent.element as unknown as HTMLDivElement)?.innerHTML
+      const buttonComponent = new blockClass({ text: textData }),
+        res = (buttonComponent.element as HTMLDivElement)?.innerHTML
 
       expect(res).toBe(textData)
     })
 
     it("Эвент тест", () => {
-      const handler = jest.fn()
-      const buttonComponent = new blockClass({
-        text: "I am button!", events:
-          { click: handler }
-      })
+      const handler = jest.fn(),
+        buttonComponent = new blockClass({
+          text: textData, events:
+            { click: handler }
+        }), event = new MouseEvent("click");
 
-      const event = new MouseEvent("click");
-      (buttonComponent.element as unknown as HTMLDivElement)?.dispatchEvent(event)
+      (buttonComponent.element as HTMLDivElement)?.dispatchEvent(event)
 
       expect(handler).toHaveBeenCalledTimes(1)
     })
 
+    it("Проверка метода .show", () => {
+      const buttonComponent = new blockClass({ text: textData})
+
+      buttonComponent.show()
+      const res = (buttonComponent.element as HTMLDivElement)?.classList.contains('display-block')
+
+      expect(res).toBe(true)
+    })
+
+    it("Проверка метода .hide", () => {
+      const buttonComponent = new blockClass({ text: textData })
+
+      buttonComponent.show()
+      let res = (buttonComponent.element as HTMLDivElement)?.classList.contains('display-block')
+
+      if (res) {
+        buttonComponent.hide()
+        res = (buttonComponent.element as HTMLDivElement)?.classList.contains('display-block')
+      }
+
+      expect(res).toBe(false)
+    })
+
     it("Сэт Пропс", () => {
-      const buttonComponent = new blockClass({ text: "TestText" })
-      const renderSpy = jest.spyOn(buttonComponent, "setProps")
+      const buttonComponent = new blockClass({ text: textData }),
+        renderSpy = jest.spyOn(buttonComponent, "setProps")
 
       renderSpy.mockClear()
       buttonComponent.setProps({ text: "updated" })
